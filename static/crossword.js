8192b1c492bf3afd9240dbd7e37b74d5227d9d77
LukeLaLima <lucasdlalima@ibm.com>
2025-09-17 10:17:01 -0400

    Added numbers to cells

--- static/crossword.js
+++ static/crossword.js
@@ -10,10 +10,47 @@ function crossword_create_grid() {
   // Create grid cells
   for (let row=0; row<grid_length; row++){
     for (let col=0; col<grid_length; col++) {
+      // Creates new cell
       const cell = document.createElement("div");
       cell.className="cell";
       cell.id = `cell-${row}-${col}`;
+
+      // Adds top-left number space
+      const cellnum = document.createElement("span");
+      cellnum.className = "cell-number";
+      cellnum.textContent = ""; // is set later
+      cell.appendChild(cellnum);
+
+      // Appends
       container.appendChild(cell)
     }; // for col
   }; //for row
 }; //function
+
+// Adds numbers to upper-left corner of cells
+function crossword_add_numbers() {
+  let index = 1;
+  const entries = crossword_json.entries;
+
+  // Create a lookup map for fast acess
+  const cluemap = new Map();
+  entries.forEach(entry => {
+    const key = `${entry.origin.row}-${entry.origin.col}-${entry.orientation}`;
+    cluemap.set(key, true);  
+  });
+
+  // Iterates over all cells & stamps number if clue exists
+  for (let row=0; row<grid_length; row++) {
+    for(let col=0; col<grid_length; col++){
+      const has_across = cluemap.has(`${row}-${col}-across`);
+      const has_down = cluemap.has(`${row}-${col}-down`);
+
+      if (has_across || has_down) {
+        const cell = document.getElementById(`cell-${row}-${col}`);
+        const numspan = cell.querySelector(".cell-number");
+        numspan.textContent = index;
+        index+=1;
+      } // if
+    } // for
+  } //for 
+} // function
--- static/main.js
+++ static/main.js
@@ -1,2 +1,3 @@
 // Anchor at end of html code
 window.onload = crossword_create_grid();
+window.onload = crossword_add_numbers();
