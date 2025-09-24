c69672948934c739217ef0555b284e6f1d53f0e6
LukeLaLima <lucasdlalima@ibm.com>
2025-09-24 11:45:10 -0400

    Added tile info to export json

--- src/export_clues_to_json.py
+++ src/export_clues_to_json.py
@@ -1,21 +1,38 @@
 import json
 
-filename = "./crossword_export_debug.json"
+filename = "./crossword_export.json" # output json file
 
 def export_clues_to_json(clues, size):
 	# Convert each clue to a dictionary
 	data = {"grid_length": size, "entries": None}
 	entries = []
 
-	# Builds entires value
+	# Builds entires & tile values
 	for clue in clues:
-		clue_data = {
-			"word": clue.word,
-			"origin": {"row": clue.origin[0], "col": clue.origin[1]},
-			"orientation": clue.orient,
-			"prompt": clue.prompt
+		# Variables 
+		word, row, col = clue.word, clue.origin[0], clue.origin[1]
+		orientation, prompt = clue.orient, clue.prompt
+
+		# Adds entry
+		entry = {
+			"word": word,
+			"origin": {"row": row, "col": col},
+			"orientation": orientation,
+			"prompt": prompt
 		}
-		entries.append(clue_data)
+
+		# Determines which cells are used
+		used_cells = []
+		if orientation == "across":
+			print(f"word: {word}, row: {row}, col: {col}")
+			for c in range(len(word)):
+				used_cells.append([row, col+c])
+		elif orientation == "down":
+			for r in range(len(word)):
+				used_cells.append([row + r, col])
+
+		entry["used_cells"] = used_cells
+		entries.append(entry)
 
 	data["entries"] = entries
 
